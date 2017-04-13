9c8cfe79720ea13bc09267bdf2fe082f4b87f843
totcw <totcw@qq.com>
2017-04-13 11:41:38 +0800

    使用vlayout修改了办理信用卡

--- app/src/main/java/com/betterda/betterdapay/activity/BanLiActivity.java
+++ app/src/main/java/com/betterda/betterdapay/activity/BanLiActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
 import com.betterda.betterdapay.R;
+import com.betterda.betterdapay.adapter.BanliCreditBannerAdapter;
+import com.betterda.betterdapay.adapter.BanliCreditItemAdapter;
+import com.betterda.betterdapay.adapter.BanliCreditThemeAdapter;
 import com.betterda.betterdapay.view.NormalTopBar;
 import com.zhy.base.adapter.ViewHolder;
 import com.zhy.base.adapter.recyclerview.CommonAdapter;
@@ -50,31 +56,20 @@ public class BanLiActivity extends BaseActivity {
         list.add(null);
         list.add(null);
         list.add(null);
-        mRvBanli.setLayoutManager(new LinearLayoutManager(getmActivity()));
+        VirtualLayoutManager virtualLayoutManager = new VirtualLayoutManager(getmActivity());
+        mRvBanli.setLayoutManager(virtualLayoutManager);
         mRvBanli.addItemDecoration(new DividerItemDecoration(getmActivity(),DividerItemDecoration.VERTICAL_LIST));
-        mRvBanli.setAdapter(new CommonAdapter<String>(getmActivity(),R.layout.item_rv_tyepcard,list) {
-            @Override
-            public void convert(ViewHolder holder, String o) {
-
-            }
-
-
-        });
+//设置回收复用池大小，（如果一屏内相同类型的 View 个数比较多，需要设置一个合适的大小，防止来回滚动时重新创建 View）：
+        RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
+        mRvBanli.setRecycledViewPool(viewPool);
+        viewPool.setMaxRecycledViews(0, 10);
+        //加载数据时有两种方式:一种是使用 DelegateAdapter, 可以像平常一样写继承自DelegateAdapter.Adapter的Adapter, 只比之前的Adapter需要多重载onCreateLayoutHelper方法。 其他的和默认Adapter一样。
+        DelegateAdapter delegateAdapter = new DelegateAdapter(virtualLayoutManager);
+        //给rv 设置 一个adapter
+        mRvBanli.setAdapter(delegateAdapter);
+        delegateAdapter.addAdapter(new BanliCreditBannerAdapter(getmActivity(),new LinearLayoutHelper(),1));
+        delegateAdapter.addAdapter(new BanliCreditThemeAdapter(getmActivity(),new LinearLayoutHelper(),1));
+        delegateAdapter.addAdapter(new BanliCreditItemAdapter(getmActivity(),new LinearLayoutHelper(),list));
     }
 
-    @OnClick({R.id.linear_banli_xingye, R.id.linear_banli_xbk, R.id.linear_banli_adls, R.id.linear_banli_lbs, R.id.linear_banli_more})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.linear_banli_xingye:
-                break;
-            case R.id.linear_banli_xbk:
-                break;
-            case R.id.linear_banli_adls:
-                break;
-            case R.id.linear_banli_lbs:
-                break;
-            case R.id.linear_banli_more:
-                break;
-        }
-    }
 }
